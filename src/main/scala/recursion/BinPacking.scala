f3e0a9fc2f392ff6620a567713e055e0baf061d5
TempixTL <tempixtl@icloud.com>
2018-04-05 09:59:12 -0500

    Added recursion

---
+++ src/main/scala/recursion/BinPacking.scala
@@ -0,0 +1,39 @@
+package recursion
+
+object BinPacking extends App {
+  def packBins(items: Array[Int], bins: Array[Int]): Boolean = {
+    
+    def helper(i: Int): Boolean = {
+      if (i >= items.length) true
+      else {
+        var ret = false
+        for (j <- bins.indices) {
+          if (items(i) <= bins(j)) {
+            bins(j) -= items(i)
+            ret ||= helper(i+1)
+            bins(j) += items(i)
+          }
+        }
+        ret
+      }
+    }
+    
+    def helper2(i: Int): Boolean = {
+      if (i >= items.length) true
+      else {
+        bins.indices.exists {j =>
+          if (items(i) <= bins(j)) {
+            bins(j) -= items(i)
+            val h = helper(i+1)
+            bins(j) += items(i)
+            h
+          } else false
+        }
+      }
+    }
+    
+    helper(0)
+  }
+  
+  println(packBins(Array(6,3,3),Array(5,7)))
+}
